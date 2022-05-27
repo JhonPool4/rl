256d8a39b3b1e355ff36508f0ad0fb65f2f4d969
wilian <booker.schelhaas@gmail.com>
2022-05-27 17:56:38 -0300

    init file

---
+++ rl_utils/__init__.py
@@ -0,0 +1,4 @@
+from .logger import Logger
+from .memory_buffer import MemoryBuffer
+from .rl_networks import DoubleQNetwork, GaussianPolicyNetwork
+from .color_print import print_info, print_warning
