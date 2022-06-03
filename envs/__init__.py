bd3280a30f43d76115369d9cf270dae92f11ee22
JhonPool4 <jhoncharaja@gmail.com>
2022-06-03 17:31:06 -0300

    add ArmEnv2D model

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,4 +1,4 @@
 from .cartpole import ContinuousCartPoleEnv
 #from .custom_arm import CustomArmEnv
 from .custom_osim_model import CustomOsimModel
-from .custom_arm_v2 import CustomArmEnv
+from .custom_arm import ArmEnv2D
