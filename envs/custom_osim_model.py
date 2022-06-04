0f1b2a3652b6ef8f21dca2bd0c488a51737b1f34
JhonPool4 <jhoncharaja@gmail.com>
2022-06-04 11:07:22 -0300

    with and option to show goal

--- envs/custom_osim_model.py
+++ envs/custom_osim_model.py
@@ -3,10 +3,11 @@ import opensim as osim
 from rl_utils import print_warning
 
 class CustomOsimModel(object):
-    def __init__(self, model_path=None, visualize=False, integrator_accuracy = 5e-5, step_size=0.01):
+    def __init__(self, model_path=None, visualize=False, integrator_accuracy = 5e-5, step_size=0.01, add_bullet=False):
         # simulation parameters
         self._int_acc = integrator_accuracy
         self._step_size = step_size
+        self._add_bullet = add_bullet
         # create model from .osim file
         self._model = osim.Model(model_path)
         # enable the visualizer
@@ -33,9 +34,34 @@ class CustomOsimModel(object):
         # get func handle
         self._control_functions= self._brain.get_ControlFunctions()
         
+
+        if self._add_bullet:
+            # Body: https://simtk.org/api_docs/opensim/api_docs/classOpenSim_1_1Body.html
+            # humerus link
+            goal_body = osim.Body("goal_body", # name
+                                    0.0001, # mass
+                                    osim.Vec3(0), # center of mass
+                                    osim.Inertia(1,1,1,0,0,0)) # inertia's moment [Ixx Iyy Izz Ixy Ixz Iyz]
+            # add display geometry
+            _geom = osim.Ellipsoid(0.025,0.025,0.025)
+            _geom.setColor(osim.Orange)
+            goal_body.attachGeometry(_geom)                                
+            # PlanarJoint: https://simtk.org/api_docs/opensim/api_docs/classOpenSim_1_1PlanarJoint.html
+            self.goal_joint = osim.PlanarJoint("goal_joint", # name
+                                    self._model.getGround(), # parent frame
+                                    osim.Vec3(0,0,0), # location
+                                    osim.Vec3(0,0,0), # rotation
+                                    goal_body, # child frame
+                                    osim.Vec3(0,0,-0.25), # location
+                                    osim.Vec3(0,0,0)) # rotation           
+
+            # add goal body and joint to model
+            self._model.addBody(goal_body)
+            self._model.addJoint(self.goal_joint)
+            
         # initialize the model and check model consistency (important!)
-        self._model.initSystem()        
-        self._state = None # model's state
+        self._model.initSystem()
+        self._state = None  # model's state
         self._manager = None # model's manager
         self._sim_timesteps = 0 # number of simulation steps      
         
@@ -67,13 +93,22 @@ class CustomOsimModel(object):
         self._manager.initialize(self._state)
 
 
-    def reset(self, init_pos=None):
+    def reset(self, init_pos=None, bullet_pos=None):
         # set initial position
         if init_pos is not None:
             self._joints.get("r_shoulder").upd_coordinates(0).setDefaultValue(init_pos["r_shoulder"])
             self._joints.get("r_elbow").upd_coordinates(0).setDefaultValue(init_pos["r_elbow"])
+        
         # compute initial state (important!)
         self._state = self._model.initializeState()
+        if bullet_pos is not None:
+            #_joints = self._model.getJointSet() # get joints
+            #goal_joint = self._model.getJointSet().get("goal_joint")
+            self.goal_joint.get_coordinates(1).setValue(self._state, bullet_pos['x'], False)
+            self.goal_joint.get_coordinates(2).setLocked(self._state, False)
+            self.goal_joint.get_coordinates(2).setValue(self._state, bullet_pos['y'], False)
+            self.goal_joint.get_coordinates(2).setLocked(self._state, True)
+
         # compute length of fibers based on the state (important!)
         self._model.equilibrateMuscles(self._state)
         # simulation parameters
