a540870264fbb6c9de738cc4ea9f7334abf0035d
wilian <booker.schelhaas@gmail.com>
2022-06-13 11:09:47 -0300

    without old environments

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,4 +1,2 @@
 from .cartpole import ContinuousCartPoleEnv
-#from .custom_arm import CustomArmEnv
-from .custom_osim_model import CustomOsimModel
-from .custom_arm import ArmEnv2D
+from .arm2DEnv import Arm2DEnv
