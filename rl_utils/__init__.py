12863a04b1f1eaecfd3f9ca22c9221a1583cc48e
JhonPool4 <jhoncharaja@gmail.com>
2022-07-25 12:49:29 -0300

    without plotter

--- rl_utils/__init__.py
+++ rl_utils/__init__.py
@@ -2,7 +2,6 @@ from .logger import Logger
 from .memory_buffer import MemoryBuffer
 from .rl_networks import DoubleQNetwork, GaussianPolicyNetwork
 from .color_print import print_info, print_warning
-from .plotter import Plotter
 from .inverse_kin_utils import shuffle_tensor
 from .inverse_kin_utils import get_min_max_cartesian
 from .inverse_kin_utils import get_rand_obs
